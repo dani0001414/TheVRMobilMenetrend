bd7038eb517f5c9f91d87e7388a4e5914cf98714
dani0001414 <dani0001414@gmail.com>
2018-03-24 22:40:59 +0100

    d
    d

--- theme.js
+++ theme.js
@@ -1,1 +1,3 @@
-document.getElementsByTagName("HTML")[0].innerHTML=document.getElementsByTagName("HTML")[0].innerHTML.replace(/TheVR Happy Hour/g , "Dövíár bódog óra");
+function themes() {
+   document.getElementsByTagName("HTML")[0].innerHTML=document.getElementsByTagName("HTML")[0].innerHTML.replace(/TheVR Happy Hour/g , "Dövíár bódog óra");
+}
