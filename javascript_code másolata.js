352784f4f841f78a9e6905f1b7cbe12ab8f45d0e
dani0001414 <dani0001414@gmail.com>
2018-06-20 12:23:42 +0200

    d
    d

--- javascript_code másolata.js
+++ javascript_code másolata.js
@@ -2,17 +2,17 @@
 
 //Weboldalt felépítő Script 
 	 	function darkt_theme() {
-			document.getElementsByClassName("content")[0].innerHTML=document.getElementsByClassName("content")[0].innerHTML.replace(/#faf9fa/g , "#0e0c13");
-			document.getElementsByClassName("content")[0].innerHTML=document.getElementsByClassName("content")[0].innerHTML.replace(/white/g , "#17141f");
-			document.getElementsByClassName("content")[0].innerHTML=document.getElementsByClassName("content")[0].innerHTML.replace(/black/g , "#c3c1c8");
-			document.getElementsByClassName("content")[0].innerHTML=document.getElementsByClassName("content")[0].innerHTML.replace(/#e5e3e8/g , "#2e2b35");
+			document.getElementsByTagName("HTML")[0].innerHTML=document.getElementsByTagName("HTML")[0].innerHTML.replace(/#faf9fa/g , "#0e0c13");
+			document.getElementsByTagName("HTML")[0].innerHTML=document.getElementsByTagName("HTML")[0].innerHTML.replace(/white/g , "#17141f");
+			document.getElementsByTagName("HTML")[0].innerHTML=document.getElementsByTagName("HTML")[0].innerHTML.replace(/black/g , "#c3c1c8");
+			document.getElementsByTagName("HTML")[0].innerHTML=document.getElementsByTagName("HTML")[0].innerHTML.replace(/#e5e3e8/g , "#2e2b35");
 		}
 		
 		function light_theme() {
-			document.getElementsByClassName("content")[0].innerHTML=document.getElementsByClassName("content")[0].innerHTML.replace(/#0e0c13/g , "#faf9fa");
-			document.getElementsByClassName("content")[0].innerHTML=document.getElementsByClassName("content")[0].innerHTML.replace(/#17141f/g , "white");
-			document.getElementsByClassName("content")[0].innerHTML=document.getElementsByClassName("content")[0].innerHTML.replace(/#c3c1c8/g , "black");
-			document.getElementsByClassName("content")[0].innerHTML=document.getElementsByClassName("content")[0].innerHTML.replace(/#2e2b35/g , "#e5e3e8");
+			document.getElementsByTagName("HTML")[0].innerHTML=document.getElementsByTagName("HTML")[0].innerHTML.replace(/#0e0c13/g , "#faf9fa");
+			document.getElementsByTagName("HTML")[0].innerHTML=document.getElementsByTagName("HTML")[0].innerHTML.replace(/#17141f/g , "white");
+			document.getElementsByTagName("HTML")[0].innerHTML=document.getElementsByTagName("HTML")[0].innerHTML.replace(/#c3c1c8/g , "black");
+			document.getElementsByTagName("HTML")[0].innerHTML=document.getElementsByTagName("HTML")[0].innerHTML.replace(/#2e2b35/g , "#e5e3e8");
 		}
 
 		function getCookie(cname) {
@@ -35,7 +35,7 @@
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {  
-    document.getElementsByClassName("content")[0].innerHTML = this.responseText;
+    document.getElementsByTagName("HTML")[0].innerHTML = this.responseText;
     if (user == "dark") { 
 		darkt_theme();
     }
@@ -43,7 +43,7 @@
 	
 	}
   };
-  xhttp.open("GET", "https://script.google.com/macros/s/AKfycbzWgMmzATZ8KYDXk0EqPnJ0YtP09WTfoeqsVU-SwgHIWx-VX7Y/exec", true);
+  xhttp.open("GET", "https://script.google.com/macros/s/AKfycbyn-ObGJyWxvfQqMTW3RdjuLbm_gZrJ816wQhvLRkRMFIewGKU/exec", true);
   xhttp.send();
 
 	if (user == "dark") { 
