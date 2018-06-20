071f9c8be1a1eceb71a8e9047a8cb37a17dbc5bd
dani0001414 <dani0001414@gmail.com>
2018-06-20 12:34:54 +0200

    d
    d

--- javascript_code.js
+++ javascript_code.js
@@ -35,7 +35,7 @@
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {  
-    document.getElementsByClassName("content")[0].innerHTML = this.responseText;
+    document.getElementsByClassName("content").innerHTML = this.responseText;
     if (user == "dark") { 
 		darkt_theme();
     }
