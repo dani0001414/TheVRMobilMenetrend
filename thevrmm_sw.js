23076fb6d3490a57ea0d2f82fcdc7bc5a5524266
dani0001414 <dani0001414@gmail.com>
2018-07-10 18:36:43 +0200

    f
    f

--- thevrmm_sw.js
+++ thevrmm_sw.js
@@ -55,7 +55,7 @@ self.addEventListener('activate', event => {
 // from the network before returning it to the page.
 self.addEventListener('fetch', event => {
   // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
+  if ((event.request.url.startsWith(self.location.origin)) && (!event.request.url.endsWith('javascript_code.js'))) {
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
         if (cachedResponse) {
