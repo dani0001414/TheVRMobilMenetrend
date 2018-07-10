7a07bed48c8f87e03fc3c4480e014d39699b29ac
dani0001414 <dani0001414@gmail.com>
2018-07-10 15:12:18 +0200

    f
    f

--- thevrmm_sw.js
+++ thevrmm_sw.js
@@ -59,7 +59,7 @@ self.addEventListener('fetch', event => {
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
         if (cachedResponse) {
-          console.log('Cache lejárati idő:', event.request.headers.get);
+          console.log('Cache lejárati idő:', event.request.headers.getAll());
           console.log('nem működik a fenti vagy nem lépbeide?');
           console.log(cachedResponse);
           return cachedResponse;
