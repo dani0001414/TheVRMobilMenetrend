f8d694714b3a855f43129542d96d35ed65858c99
dani0001414 <dani0001414@gmail.com>
2018-07-14 03:36:19 +0200

    d
    d

--- thevrmm_sw.js
+++ thevrmm_sw.js
@@ -92,7 +92,7 @@ self.addEventListener('fetch', event => {
           if (time != null) { cached_time = time; }
           return cachedResponse;
         }
-        console.log('hmm:', time);
+        
         return caches.open(RUNTIME).then(cache => {
           return fetch(event.request).then(response => {
             // Put a copy of the response in the runtime cache.
@@ -112,3 +112,5 @@ self.addEventListener('fetch', event => {
     }
   }
 })
+
+console.log('hmm:', time);
