787ec2253cd2b42f2bf95100e826cb382c7d8a57
dani0001414 <dani0001414@gmail.com>
2018-08-23 09:09:01 +0200

    f
    f

--- javascript_code_beta.js
+++ javascript_code_beta.js
@@ -437,10 +437,7 @@ function HtmlStart() {
 		var brId = i + "_br";
 		var blankCover = "https://static-cdn.jtvnw.net/twitch-event-images-v2/default/town-320x180";
 		
-		if (cookieSettings != 1) {
-			streamStart[i] = Timestamp(events[i].node.startAt);
-			streamEnd[i] = Timestamp(events[i].node.endAt);
-		}
+		
 
 		if (cookieSettings == 1) {
 			//////
