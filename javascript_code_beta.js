9d3687019a4b2d5aa9e23d663fc08570b1230b54
dani0001414 <dani0001414@gmail.com>
2018-08-30 12:18:58 +0200

    f
    f

--- javascript_code_beta.js
+++ javascript_code_beta.js
@@ -234,14 +234,15 @@ function HttpGetFeature(url, callback) {
 }
 
 function HttpGetNorm(url) {
-
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function () {
-		if (this.readyState == 4 && this.status == 200) {
-		}
-	};
-	xhttp.open("GET", url, true);
-	xhttp.send();
+	if (cookieSettings == 1) {
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+			}
+		};
+		xhttp.open("GET", url, true);
+		xhttp.send();
+	}
 }
 
 /*Változtatás : streamEndZeroElement, streamStartZeroElement változók deklarálása itt. */
