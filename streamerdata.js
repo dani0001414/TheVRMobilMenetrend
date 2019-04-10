d09f88a07c2852d55d8246336c4af8306d3b581f
dani0001414 <dani0001414@gmail.com>
2019-04-10 17:34:57 +0200

    Create streamerdata.js

---
+++ streamerdata.js
@@ -0,0 +1,9 @@
+/**MobilBarát Menetrend Testreszabása. */
+/*Streamer adatok megadása*/
+
+var streamer = "wearethevr";
+var noEventsPic = "https://i.imgur.com/5dZn6sc.png";
+var offlinePic = "https://i.imgur.com/5dZn6sc.png";
+var offlineText = "Kihúztad az UTP Kábelt!";
+var noEventsText = "Jelenleg nincs egy stream sem a menetrendben! Elszívták az UTP-vel együtt! <img src=\"http://static-cdn.jtvnw.net/emoticons/v1/25/1.0\" alt=\"23\"><br>Hamarosan újabb szálítmány!";
+
